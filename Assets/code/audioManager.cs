19649df8a3c1a0003bad7c282a8dc38b34f3d980
Einstephener <kihyuk0201@gmail.com>
2023-10-30 20:20:46 +0900

    if game stop, bgm stop

--- Assets/code/audioManager.cs
+++ Assets/code/audioManager.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class audioManager : MonoBehaviour
 {
     public AudioSource audioSource;
     public AudioClip bgmusic;
+    public Text timeTxt;
+    bool isEnd = false;
+    float time = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +23,24 @@ public class audioManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Time.timeScale = 1.0f;
+        time += Time.deltaTime;
+
+        if (time > 30.0f)
+        {
+            isEnd = true;
+
+            if(isEnd == true)
+            {
+                audioSource.Pause();
+            }
+            else
+            {
+                audioSource.Play();
+            }
+
+        }
+        
         
     }
 }
