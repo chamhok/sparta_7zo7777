9fadef01e8d56b7328de6d4dd3b712ae6db4bbb6
Einstephener <kihyuk0201@gmail.com>
2023-10-30 20:46:53 +0900

    다시하기버튼, 홈으로 버튼

---
+++ Assets/code/goHome.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class goGallery : MonoBehaviour
+{
+    public void GoHome()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
