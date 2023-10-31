9637eeb83eb4a042d0e5b291229157b35972545f
Einstephener <kihyuk0201@gmail.com>
2023-10-31 17:08:36 +0900

    갤러리에서 홈화면
    갑자기 사라져서 다시 만들었습니다

---
+++ Assets/code/goBack.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goBack : MonoBehaviour
+{
+    public AudioClip click;
+    public AudioSource audioSource;
+
+    public void goStartScene()
+    {
+        audioSource.PlayOneShot(click);
+        //클릭 사운드
+        SceneManager.LoadScene("StartScene");
+        //시작화면으로 이동
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
