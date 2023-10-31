721932312d421eb5a8325ce30686ecedf7a65eed
YongTaeJ <g_o_robot@naver.com>
2023-10-31 15:39:42 +0900

    깨진 글자 한글로

--- Assets/code/GalleryManager.cs
+++ Assets/code/GalleryManager.cs
@@ -9,28 +9,27 @@ public class GalleryManager : MonoBehaviour
 
 
 
-    // �������δ� ī�� ���� �´���Ȯ���ϴ°ſ��� ī�尡 �¾����� ������������ ī���� ����Ʈ�߰� ������ ī��� �׳� ��?
+	// 수집여부는 카드 두장 맞는지확인하는거에서 카드가 맞았을때 수집되지않은 카드라면 이펙트뜨고 수집한 카드면 그냥 들어감?
 
 
-   // AudioSource audioSource;
-   // Animator anim; // ���ӸŴ��� �ִϸ�����
+	// AudioSource audioSource;
+	// Animator anim; // 게임매니저 애니메이터
 
+	public GameObject popupObj;
+    GalleryPopup[] galleryPopups; // 팝업창 애니메이터들
 
-    public GameObject popupObj;
-    GalleryPopup[] galleryPopups; // �˾�â �ִϸ����͵�
-
-
-    /*
+	/*
       
-     * CG ������ŭ ������ ��ư �����հ� �˾�â �������� ��Ʈ��D�� �ø��� �˾�â ������ �������ش�.
+     * CG 종류만큼 갤러리 버튼 프리팹과 팝업창 프리팹을 컨트롤D로 늘리고 팝업창 내용을 수정해준다.
      * 
-     * �˾�â�� ��ư ENUM���� CG �̸� �߰��ؾ���. ���߿� CG �رݿ��� �������� �ʿ�
+     * 팝업창과 버튼 ENUM에도 CG 이름 추가해야함. 나중에 CG 해금여부 읽을때도 필요
+
     
      
      */
 
 
-    void Awake()
+	void Awake()
     {
 
       //  anim = GetComponent<Animator>();
@@ -51,8 +50,8 @@ public class GalleryManager : MonoBehaviour
 
     }
 
-    public void PopupOpen(WhoIs name) // �˾�â����. �ڱ� �ѹ� �־ ����. �ѹ����´� �˾�â ���µ�.
-    {
+    public void PopupOpen(WhoIs name) // 팝업창열기. 자기 넘버 넣어서 실행. 넘버에맞는 팝업창 오픈된.
+	{
 
         for (int i = 0; i < galleryPopups.Length; i++)
         {
