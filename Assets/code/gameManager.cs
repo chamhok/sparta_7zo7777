9ac84bdabfa7685be2e19e7529c83f732ac25f73
Einstephener <kihyuk0201@gmail.com>
2023-10-31 10:21:29 +0900

    실패사운드 추가

--- Assets/code/gameManager.cs
+++ Assets/code/gameManager.cs
@@ -12,6 +12,7 @@ public class gameManager : MonoBehaviour
     public AudioClip bgmusic;
     public AudioClip defeat;
     public AudioClip success;
+    public AudioClip wrong;
 
     public Text timeTxt;
     public Text tryTxt;
@@ -157,6 +158,7 @@ public class gameManager : MonoBehaviour
         }
         else
         {
+            audioSource.PlayOneShot(wrong);
             setMatchTxt("꽝!!!");
             firstCard.GetComponent<card>().closeCard();
             secondCard.GetComponent<card>().closeCard();
