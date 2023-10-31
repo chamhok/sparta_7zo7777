be98e99e68b181ba460daf3fb0503b7516994048
JacksonPepperoni <mashpota1014@gmail.com>
2023-10-31 11:34:04 +0900

    갤러리 중간저장

--- Assets/code/GalleryManager.cs
+++ Assets/code/GalleryManager.cs
@@ -1,37 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
+using static GalleryBtn;
 
 public class GalleryManager : MonoBehaviour
 {
 
 
+
     // 수집여부는 카드 두장 맞는지확인하는거에서 카드가 맞았을때 수집되지않은 카드라면 이펙트뜨고 수집한 카드면 그냥 들어감?
-    AudioSource audioSource;
+
+
+   // AudioSource audioSource;
+   // Animator anim; // 게임매니저 애니메이터
+
+
+    public GameObject popupObj;
+    GalleryPopup[] galleryPopups; // 팝업창 애니메이터들
+
+
+
+    void Awake()
+    {
+
+      //  anim = GetComponent<Animator>();
+     //   audioSource = GetComponent<AudioSource>();
+    }
 
 
     void Start()
     {
-        
+
+        galleryPopups = new GalleryPopup[popupObj.transform.childCount];
+
+
+        for (int i = 0; i < popupObj.transform.childCount; i++)
+        {
+            galleryPopups[i] = popupObj.transform.GetChild(i).gameObject.GetComponent<GalleryPopup>();
+        }
+
     }
 
-    void Update()
+    public void PopupOpen(WhoIs name) // 팝업창열기. 자기 넘버 넣어서 실행. 넘버에맞는 팝업창 오픈된.
     {
-        
-    }
 
+        for (int i = 0; i < galleryPopups.Length; i++)
+        {
+            if ((int)galleryPopups[i].whoIs == (int)name)
+            {
+                galleryPopups[i].Open();
+            }
 
+        }
 
-    public void PopupOpen(int num) // 팝업창열기. 자기 넘버 넣어서 실행. 넘버에맞는 팝업창 오픈된.
-    { 
-    
-    
-    }
-
-    public void PopupClose() // 팝업창 닫기에 연결
-    { 
-    
-    
     }
 
 }
